d18e3a8d2592d70d0631e14c81f47cbfff1a5c47
AniAgyei Daniel <danielaniagyei7@gmail.com>
2025-02-25 10:31:14 +0000

    splashScreen

---
+++ app/src/main/java/com/example/salonapplication/splashScreen.java
@@ -0,0 +1,33 @@
+package com.example.salonapplication;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.content.Intent;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        // Delay for 2 seconds and then navigate to WelcomeActivity
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(splashScreen.this, RegisterPage.class);
+            startActivity(intent);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish(); // Close the splash screen
+        },2000);
+
+
+
+    }
+}
