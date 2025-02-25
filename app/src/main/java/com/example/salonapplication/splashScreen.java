8e0e60f859fda1e555b148a29235dd67d89f74b6
AniAgyei Daniel <danielaniagyei7@gmail.com>
2025-02-25 10:52:28 +0000

    loader

--- app/src/main/java/com/example/salonapplication/splashScreen.java
+++ app/src/main/java/com/example/salonapplication/splashScreen.java
@@ -7,9 +7,6 @@ import android.content.Intent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class splashScreen extends AppCompatActivity {
 
